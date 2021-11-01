fd76fc9769d3545668904080785d69deeedade6a
woolarinet <pjwpjw103@naver.com>
2021-11-02 00:02:06 +0900

    count 추가

--- pages/api/blog/[id].js
+++ pages/api/blog/[id].js
@@ -4,12 +4,12 @@ const query = require('../../../config/graphql')
 export default async function blog(req, res) {
   const router = req.query
   console.log(router)
-  const cate = await blogFunctions.getEntries(query.category)
+  const cate = await blogFunctions.getAll(query.allPost)
   const info = await blogFunctions.getDetail(query.post(router.cate, router.id))
   console.log('info: ', info)
   if (info !== false) {
     res.status(200).json({
-      cate: cate,
+      cate: cate.cate,
       detail: info,
     })
   }
--- pages/api/blog/category.js
+++ pages/api/blog/category.js
@@ -2,11 +2,12 @@ const query = require('../../../config/graphql')
 const blogFunctions = require('./blogFunction')
 
 export default async (req, res) => {
-  const cate = await blogFunctions.getEntries(query.category)
+  const cate = await blogFunctions.getAll(query.allPost)
+  console.log('cate', cate)
   const result = await blogFunctions.getPostInCate(query.postsInCate(req.query.cate))
   if (result !== false) {
     res.status(200).json({
-      entries: cate,
+      entries: cate.cate,
       posts: result,
     })
   } else {
